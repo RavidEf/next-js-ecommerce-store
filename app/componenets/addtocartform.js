3215d0f319bd3b348c93a8df584c4e91ab59aa3f
Ravid Efroni <ravidefroni@gmail.com>
2024-10-05 07:55:58 +0200

    Add input dropdown field to the form

--- app/componenets/addtocartform.js
+++ app/componenets/addtocartform.js
@@ -11,6 +11,17 @@ export default function FormAddToCart() {
     <div>
       <form onSubmit={handelSubmit}>
         <p>Item quntity: </p>
+        <select id="quantity-to-cart">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+        </select>
         <AddToCartButton />
       </form>
     </div>
